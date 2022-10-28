00d6af3f52d117f288574266c49cfab0145e4448
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-10-28 18:10:44 +0200

    @override php

---
+++ footer.php
@@ -0,0 +1,22 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    
+    <nav class="navbar fixed-bottom" style="background-color: #e3f2fd;">
+        <div class="container-fluid text-center d-flex justify-content-center">
+            <div class="d-flex justify-content-center">
+                <p class="impressum">@SpitzerMatyus GmbH</p>
+                <p>|</p>   
+                <a class="impressum" href="impressum.php">Impressum</a>
+                <p>|</p>
+                <a class="impressum" href="faq.php">FAQ</a>
+            </div>
+            <br>    
+        </div>
+    </nav>
+    <div class="fixed-bottom d-flex justify-content-center">
+        
+    </div>
+    
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,41 @@
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg " style="background-color: #e3f2fd;">
+    <a class="navbar-brand" href="#">WEBHOTEL</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a href="index.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=="index.php"){echo 'active';}else{echo '';} ?>">Home</a>
+        </li>
+        <li class="nav-item">
+        <a href="login.php" class="nav-link <?= (basename($_SERVER['PHP_SELF'])=="login.php")?'active':''; ?>">Log in!</a>
+        </li>
+        <li class="nav-item">
+        <a href="signup.php" class="nav-link <?= (basename($_SERVER['PHP_SELF'])=="signup.php")?'active':''; ?>">Sign up!</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
