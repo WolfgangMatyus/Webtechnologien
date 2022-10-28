e82c33f096f934f7235dd105b7f82e5920114ae6
WolfgangMatyus <wi21b055@technikum-wien.at>
2022-10-28 18:46:19 +0200

    nav centered

--- .php_tmp/nav.php
+++ .php_tmp/nav.php
@@ -1,11 +1,11 @@
 <div class="container-fluid">
-    <nav class="navbar navbar-expand-lg " style="background-color: #e3f2fd;">
-    <a class="navbar-brand" href="index.php">WEBHOTEL</a>
+    <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+    <a class="navbar-brand col-md-2" href="index.php">WEBHOTEL</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav col-md-4 ms-md-auto mb-2 mb-lg-0 justify-content-center">
+      <ul class="navbar-nav col-md-4.5 ms-md-auto mb-2 mb-lg-0 justify-content-center">
         <li class="nav-item">
           <a href="index.php" class="nav-link <?php if(basename($_SERVER['PHP_SELF'])=="index.php"){echo 'active';}else{echo '';} ?>">Home</a>
         </li>
@@ -16,7 +16,7 @@
         <a href="signup.php" class="nav-link <?= (basename($_SERVER['PHP_SELF'])=="signup.php")?'active':''; ?>">Sign up!</a>
         </li>
       </ul>
-      <form class="col-md-3 ms-md-auto d-flex" role="search">
+      <form class="col-md-3.5 ms-md-auto d-flex" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
