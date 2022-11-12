17167c36a00006e9f07f10d9c76066071b75825f
Robert <lois.spitzer@gmail.com>
2022-11-12 23:51:41 +0100

    edit index

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-    include 'header.php';
+    include_once 'header.php';
 ?>
 
+    <?php
+        if(isset($_SESSION["useruid"])) {
+            echo "<p>Welcome " . $_SESSION["useruid"] . "</p>";
+        }
+    ?>
     <div class="container-fluid d-flex justify-content-center" style="margin: 20px; padding: 20px 20px 20px 20px; background-image: url(pics/fh_technikum_kv.jpg); background-position: center; background-repeat: no-repeat;">      
         <div class="jumbotron">
                 <h1 class="display-4">Welcome to our WebHotel!</h1>
@@ -23,6 +28,6 @@
     </div>
     <hr>
 <?php
-    include 'footer.php';
+    include_once 'footer.php';
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
