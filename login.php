647b5b44b629212a119da3d72b4c42449ee29d1a
Robert <lois.spitzer@gmail.com>
2022-11-13 00:01:19 +0100

    implementing form actions into login and signup

--- login.php
+++ login.php
@@ -1,26 +1,44 @@
 <?php
- include_once 'header.php';
+    include 'header.php';
 ?>
-
-<section class="signup-form">
-    <h2>Log In</h2>
-    <form action="included//login.inc.php" method="post">
-        <input type="text" name="uid" placeholder="Username/Email..."> 
-        <input type="password" name="pwd" placeholder="Password..."> 
-        <button type="submit" name="submit">Log In</button>
-    </form>
-    <?php
-    if(isset($_GET["error"])){
-        if($_GET["error"]== "emptyinput"){
-            echo "<p>Fill in all fields!</p>";
-        }
-        else if($_GET["error"]== "wronglogin"){
-            echo "<p>Incorrect Credentials!</p>";
-        }
-    }     
-?>
-</section>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Login</h1>
+            <hr>
+        </div>
+    </div>
+</div>
+<div class = "wrapper" form action="included//login.inc.php" method="post">
+    <div class="container d-flex justify-content-center" style="padding: 20px;">
+    <div class="card border-primary " style="max-width: 30rem;">
+        <div class="card-header bg-transparent border-primary">Header</div>
+        <div class="card-body text-success">
+    <h5 class="card-title">Success card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Remember Login</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>  
+</div>
+  <div class="card-footer bg-transparent border-primary">Thank You! We wish you a nice stay!</div>
+</div>
+</div>
+</div>
 
 <?php
- include_once 'footer.php';
+    include 'footer.php';
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
